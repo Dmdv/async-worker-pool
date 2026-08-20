c5bb142ae037dc9578a127001ea09a37b7913d82
Dmdv <805238+Dmdv@users.noreply.github.com>
2026-08-20 15:24:05 +0700

    chore: ignore rust target directory

--- bindings/rust/target/release/deps/awp_rs-3e1fb686966a9274.d
+++
@@ -1,6 +0,0 @@
-/Users/dima/c_lang/async-worker-pool/bindings/rust/target/release/deps/awp_rs-3e1fb686966a9274.d: src/lib.rs src/sys.rs
-
-/Users/dima/c_lang/async-worker-pool/bindings/rust/target/release/deps/awp_rs-3e1fb686966a9274: src/lib.rs src/sys.rs
-
-src/lib.rs:
-src/sys.rs:
--- bindings/rust/target/release/deps/awp_rs-feaf7905658b012e.d
+++
@@ -1,8 +0,0 @@
-/Users/dima/c_lang/async-worker-pool/bindings/rust/target/release/deps/awp_rs-feaf7905658b012e.d: src/lib.rs src/sys.rs
-
-/Users/dima/c_lang/async-worker-pool/bindings/rust/target/release/deps/libawp_rs-feaf7905658b012e.rlib: src/lib.rs src/sys.rs
-
-/Users/dima/c_lang/async-worker-pool/bindings/rust/target/release/deps/libawp_rs-feaf7905658b012e.rmeta: src/lib.rs src/sys.rs
-
-src/lib.rs:
-src/sys.rs:
